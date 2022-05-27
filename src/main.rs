64dd577ea5ae37432470880a87fcf4f8429f0f07
ollielynas <lynasollie@gmail.com>
2022-05-27 13:03:54 +1200

    Awitching to bevy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+// #![windows_subsystem = "windows"]
+
 
 use std::thread;
 use std::{
-    io::{stdout, Write},
     thread::sleep,
     time::Duration,
 };
@@ -15,11 +16,10 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
+use bevy::prelude::*;
+
+
+
 
 mod smooth_brain;
 
@@ -28,10 +28,13 @@ use std::sync::Mutex;
 
 static ARRAY: Lazy<Mutex<Vec<PhysObj>>> = Lazy::new(|| Mutex::new(vec![]));
 
+use std::fs;
+
+extern crate find_folder;
 
 
+use native_dialog::{MessageDialog, MessageType};
 
-#[derive(Debug, Clone, Copy)]
 
 
 
@@ -40,7 +43,7 @@ static ARRAY: Lazy<Mutex<Vec<PhysObj>>> = Lazy::new(|| Mutex::new(vec![]));
 //                      Variables                               |
 // -------------------------------------------------------------|
 
-
+#[derive(Copy, Clone)]
 struct PhysObj {
     x: i16,
     y: i16,
@@ -53,14 +56,19 @@ struct PhysObj {
 }
 
 
-
 impl PhysObj{
     fn gravity(&mut self) { 
-        if self.vy <= 10{
+        if self.vy <= 5{
             self.vy += 1;
-    }}
+        }else {
+            self.vy -= 1;
+        }
+    }
 
     fn collision(&mut self) {
+
+        self.gravity();
+
         if self.y - self.height as i16 <= 500 {
             if self.vy > 0 {
                 self.vy = 0;
@@ -76,16 +84,34 @@ impl PhysObj{
             self.x = 0;
         }
         if self.x + self.width as i16 > 500 {
-            self.vx = 0;
+            if self.vx > 0 {
+                self.vx = 0;
+            }
             self.x = 500 - self.width as i16;
         }
     }
     fn move_obj(&mut self) {
-        if self.iframes > 0{
+        if self.iframes > 0 {
             self.iframes -= 1;
         }
+
         self.x += self.vx;
         self.y += self.vy;
+
+
+
+        if self.vy > 0 {
+            self.vy -= 1;
+        }else if self.vy < 0 {
+            self.vy += 1;
+        }
+
+        if self.vx > 0 {
+            self.vx -= 1;
+        }else if self.vx < 0 {
+            self.vx += 1;
+        }
+
         if self.x < 0 {
             self.x = 0;
         }
@@ -94,36 +120,34 @@ impl PhysObj{
         }}
 
     fn jump(&mut self) {
-        if self.y == 500 - self.height as i16 {
+        if self.y < 495 - self.height as i16 {
             self.vy = -10;
         }
     }
 
     fn move_left(&mut self) {
-        if self.vx < -5 {self.vx += 1;
-        } else if self.vx == -5 {self.vx = -5;}
-        else {self.vx -= 1;}
+        if self.vx > -5 {self.vx += -3}
     }
 
     fn move_right(&mut self) {
-        if self.vx > 5 {self.vx -= 1;
-        } else if self.vx == 5 {self.vx = 5;}
-        else {self.vx += 1;}
+    if self.vx < 5 {self.vx += 3;}
     }
 
 
 
     fn zombie_ai(&mut self, x:i16, y:i16) -> bool {
         if self.x < x{
+            self.vx -=1;
             self.move_right();
         } if self.x > x {
+            self.vx +=1;
             self.move_left();
         }
 
-        if self.x() < x+40 
-        && (self.x()+self.width() as i16) >  x 
-        && (self.y()+self.height() as i16) > y
-        && self.y() < y+70 {
+        if self.x() <= x+40
+        && (self.x()+self.width() as i16) >=  x 
+        && (self.y()+self.height() as i16) >= y
+        && self.y() <= y+70 {
             return true
         }
         return false
@@ -137,31 +161,69 @@ impl PhysObj{
     fn width(&self) -> u8{self.width}
     fn height(&self) -> u8{self.height}
     fn iframes(&self) -> i8{self.iframes}
+
+    fn set_hp(&mut self, hp:i8){self.hp = hp;}
+    fn set_iframes(&mut self, iframes:i8){self.iframes = iframes;}
 }
+
+
 
 // -------------------------------------------------------------|
 //                      Ai                                      |
 // -------------------------------------------------------------|
 
-fn main_sim(brain:u8, render:bool) {
+fn main_sim(brain:u8, render:bool) -> BrainScore {
 
 
+    let mut matrix_str: String = std::fs::read_to_string("src/JSON/run.json").unwrap();
+    matrix_str = smooth_brain::v1::mutate(&matrix_str);
 
     let mut level_num = 1;
-
+    let mut score = 0;
 
     loop {
-        let ret:u8 = start_level(level_num, brain, render);
-        if ret == 0 {
+        let ret:Vec<i32> = start_level(level_num, brain, render, matrix_str.clone(), score);
+        score = ret[1];
+        if ret[0] == 0 {
             level_num += 1;
-        } else if ret == 1 {
-            println!("made it to level {}", level_num);
+        } else if ret[0] == 1 {
             break;
         }
     }
+
+    BrainScore {
+        brain: matrix_str.clone(),
+        score: score,
+    }
 }
 
-fn start_level(level_num: u32, brain: u8, render:bool) -> u8 {
+
+fn new_enemy(brain: u8) -> PhysObj{
+
+    const ENEMY_HEIGHT: [u8; 3] = [30, 75, 80];
+    const ENEMY_WIDTH: [u8; 3] = [60, 40, 50];
+    const ENEMY_HP: [i8; 3] = [8, 5, 10];
+
+    let mut enemy_type = rand::thread_rng().gen_range(0..ENEMY_WIDTH.len());
+
+    if brain == 0 {
+        enemy_type = 1;
+    }
+
+    return PhysObj {
+        x: rand::thread_rng().gen_range(0..400),
+        y: rand::thread_rng().gen_range(200..500),
+        vx: 0,
+        vy: 0,
+        width: ENEMY_WIDTH[enemy_type],
+        height: ENEMY_HEIGHT[enemy_type],
+        hp: ENEMY_HP[enemy_type],
+        iframes: 10,
+    };
+}
+
+fn start_level(level_num: u32, brain: u8, render:bool, matrix_s:String, mut score: i32) -> Vec<i32> {
+
     let mut player = PhysObj {
         x: 250,
         y: 250,
@@ -172,86 +234,95 @@ fn start_level(level_num: u32, brain: u8, render:bool) -> u8 {
         hp: 10,
         iframes: 0,
     };
-    player.jump();
 
 
+    let mut loop_counter = 0;
 
-    fn new_enemy(brain: u8) -> PhysObj{
+    let mut enemy_list:Vec<PhysObj> = vec![];
+    for _ in 0..level_num {
+        enemy_list.push(new_enemy(brain));
+    }
 
-        const ENEMY_HEIGHT: [u8; 3] = [30, 75, 80];
-        const ENEMY_WIDTH: [u8; 3] = [60, 40, 50];
-        const ENEMY_HP: [i8; 3] = [8, 5, 10];
+    loop { // Main Loop
 
-        let mut enemy_type = rand::thread_rng().gen_range(0..ENEMY_WIDTH.len());
+    // create loop_counter
 
-        if brain == 0 {
-            enemy_type = 1;
-        }
+    loop_counter += 1;
 
-        return PhysObj {
-            x: rand::thread_rng().gen_range(0..400),
-            y: rand::thread_rng().gen_range(200..500),
-            vx: 0,
-            vy: 0,
-            width: ENEMY_WIDTH[enemy_type],
-            height: ENEMY_HEIGHT[enemy_type],
-            hp: ENEMY_HP[enemy_type],
-            iframes: 10,
-        };
+    if loop_counter > 50000 {
+        println!("Loop Counter Exceeded");
+        drop(loop_counter);
+        return [1, score].to_vec();
     }
 
 
 
-    let mut enemy_list = [
-        &mut new_enemy(brain),
-    ];
 
 
 
+    let mut new_frame:Vec<PhysObj> = vec![];
 
+    if render {
+    new_frame.push(player.clone());
+    }
 
-
-    loop {
-    if player.hp() >= 0 {
-    player.gravity();
+    if brain == 0 {
+        score += 1; // reward brain 0 just for surviving another frame, because that is all it can do
+    }
+    
     player.collision();
-    for i in &mut enemy_list {
-        i.gravity();
-        i.collision();
-        i.move_obj();
 
+
+    for i in &mut enemy_list {
+        i.collision();
         if i.height() == 75 {
-            if player.zombie_ai(player.x(), player.y()) {
-                player.hp -= 1;
+            if i.zombie_ai(player.x(), player.y()) {
+                if player.iframes() == 0 {
+                    player.set_hp(player.hp() - 1);
+
+                    player.set_iframes(10);
+                    score -= 100;  // Punishment for being hit
+                }
             }
         }
         if render {
-            ARRAY.lock().unwrap().push(**i);
+            new_frame.push(i.clone());
         }
+        i.move_obj();
     }
+
+
 
     // return list of bool in this order to indicate weather to perform this action
     // outputs include ["move_left", "move_right", "jump", "attack_up", "attack_down", "attack"]
 
-    let actions: Vec<bool> = smooth_brain::v1::run(0, 0);
+    let actions: Vec<bool> = smooth_brain::v1::run(serde_json::from_str(&matrix_s).unwrap(), vec![player.x() as f64, enemy_list[0].x() as f64]);
+
+    if actions[0] {
+        player.move_left();
+    }
+    if actions[1] {
+        player.move_right();
+    }
 
     player.move_obj();
 
-    let mut stdout = stdout();
-
+    // add new frame to array
 
     if render {
-        stdout.flush().unwrap();
-        sleep(Duration::from_millis(100));
-        ARRAY.lock().unwrap().push(player);
         ARRAY.lock().unwrap().clear();
+        ARRAY.lock().unwrap().append(&mut new_frame);
+        sleep(Duration::from_millis(10));
     }
 
-    }else{
-        return 1;
+    if player.hp() <= 0 {
+        println!("player died: Score {}", score);
+        return [1, score].to_vec();
     }
+    if enemy_list.len() == 0 {
 
-
+        return [0, score].to_vec();
+    }
     }
 }
 
@@ -264,97 +335,112 @@ fn start_level(level_num: u32, brain: u8, render:bool) -> u8 {
 
 
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    objects: Vec<PhysObj>
+
+
+struct BrainScore {
+    brain: String,
+    score: i32,
 }
-
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-        const BG: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
-        const MG: [f32; 4] = [0.2, 0.2, 0.2, 1.0];
-        // const FG: [f32; 4] = [0.5, 0.5, 0.5, 1.0];
-
-        let arena = rectangle::square(500.0, 200.0, 500.0);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BG, gl);
-            let transform = c
-                .transform
-                .trans(0.0, 0.0);
-            rectangle(MG, arena, transform, gl);
-        });
-
-        let obj = ARRAY.lock().unwrap()[0];
-
-        for j in 0..ARRAY.lock().unwrap().len() {
-            println!("{}", j);
-
-            println!("{}", obj.y());
-            self.gl.draw(args.viewport(), |c, gl | {
-                rectangle([1.0, 0.0, 0.0, 1.0], // red
-                          [
-                            obj.x() as f64+ 500.0,
-                            obj.y() as f64+200.0,
-                            obj.width() as f64,
-                            obj.height() as f64
-                            ],
-                          c.transform, gl); 
-    });
-}
-}
-
-
-}
-
 
 fn main() {
-
-
-    for _t in 0..10 {
-        thread::spawn(|| {
-            for _i in 0..100 {
-            main_sim(0, false);
-            }
-        });
-    };
-
-
-    thread::spawn(|| {
-        for _i in 0..100 {
-        main_sim(0, true);
-        }
-    });
-
-    println!("Hello, world!");
-
 
     // different networks indicator
     // run:0, 
     let mut brain = 0;
 
-    let opengl = OpenGL::V3_2;
-
-    let mut window: Window = WindowSettings::new("spinning-square", [1050, 750])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        objects: Vec::new(),
-    };
+    // rust piston popup input 
 
 
 
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
+
+    
+    let mut thread_count:u16 = 5;
+    
+    
+    let mut rsrc_dir = std::env::current_exe()
+    .expect("Can't find path to executable");
+    rsrc_dir.pop();
+    rsrc_dir.push("JSON");
+    
+
+        
+        thread::spawn(move|| {
+            
+    println!("work Thread");
+    for generation in 0..100 { // for now its caped at 100 generations
+        println!("Generation {}", generation);
+        let mut hands = vec![
+            thread::spawn(|| {
+                {
+                main_sim(0, false)
+                }
+            }),
+        ];
+            for _ in 0..thread_count-1 { // 10 threads/ or generations are created where they will play till they die. they will then be given a score
+                hands.push( thread::spawn(|| {
+                    main_sim(0, false)
+                }));
+            };
+        
+            println!("started all threads: {}", hands.len());
+
+
+        let mut generation_result = Vec::new();
+        
+        while hands.len() > 0 { // the 10 threads are put in a list
+            let cur_thread = hands.remove(0); // moves it into cur_thread
+            generation_result.push(cur_thread.join().unwrap());
         }
+
+        println!("finished all threads: {}", generation_result.len());
+
+        // once all the threads have finished they are ranked
+        let mut best = 0;
+        for x in 0..generation_result.len() {
+            if generation_result[x].score > generation_result[best].score {
+                best = x;
+            }
+        }
+
+        let brain_lookup:Vec<String> = vec!["src/JSON/run.json".to_string()];
+
+
+        fs::write(brain_lookup[0].clone(), generation_result[best].brain.clone()).expect("Unable to write file");
+
+        println!("Best score: {}", generation_result[best].score);
+
     }
+    });
+
+
+    thread::spawn(|| {
+        println!("display Thread");
+        for _ in 0..100 {
+        main_sim(0, true);
+        }
+    });
+
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
+    .add_system(render)
+    .run();
 }
+
+fn render() {
+
+}
+
+fn setup(mut commands: Commands) {
+commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+commands.spawn_bundle(SpriteBundle {
+    sprite: Sprite {
+        color: Color::rgb(0.25, 0.25, 0.75),
+        custom_size: Some(Vec2::new(500.0, 500.0)),
+        ..default()
+    },
+    ..default()
+});
+
+}
+
--- src/smooth_brain.rs
+++ src/smooth_brain.rs
@@ -5,17 +5,16 @@ pub mod v1 {
 // outputs include ["move_left", "move_right", "jump", "attack_up", "attack_down", "attack"]
 
 use serde_derive::{Deserialize, Serialize};
-use serde_json::Result;
-use std::fs::File;
+use rand::Rng;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct InputNode {
+pub struct InputNode {
     bias:f32,
     value:f32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Node {
+pub struct Node {
     bias:f32,
     value:f32,
     weights:Vec<f32>,
@@ -23,16 +22,98 @@ struct Node {
 
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Matrix2442 {
+pub struct Matrix2442 {
     input_nodes: Vec<InputNode>,
-    hidden_nodes_1: Vec<Node>
+    hidden_nodes_1: Vec<Node>,
+    hidden_nodes_2: Vec<Node>,
+    output_nodes: Vec<Node>
 }
 
-    pub fn run(px:i16, ex:i16) -> Vec<bool> {
-        let matrix: Matrix2442 = serde_json::from_str(&std::fs::read_to_string("src/JSON/run.json").unwrap()).unwrap();
-        let mut action: Vec<bool> = vec![false, false, false, false, false, false];
-        action
+    pub fn mutate(mx: &String) -> String {
+        let mut mxo:Matrix2442 = serde_json::from_str(&mx).unwrap();
+        let mut rng = rand::thread_rng();
+
+        for i in 0..mxo.input_nodes.len() {
+            let rng_num = rng.gen_range(-5.0..5.0);
+            mxo.input_nodes[i].bias += rng_num;
+        }
+        // println!("{:?}", mxo);
+
+        for i in 0..mxo.hidden_nodes_1.len() {
+            let rng_num = rng.gen_range(-1.0..1.0);
+            mxo.hidden_nodes_1[i].bias += rng_num;
+            for j in 0..mxo.hidden_nodes_1[i].weights.len() {
+                let rng_num = rng.gen_range(-0.1..0.1);
+                mxo.hidden_nodes_1[i].weights[j] += rng_num;
+            }
+        }
+
+        for i in 0..mxo.hidden_nodes_2.len() {
+            let rng_num = rng.gen_range(-1.0..1.0);
+            mxo.hidden_nodes_2[i].bias += rng_num;
+            for j in 0..mxo.hidden_nodes_2[i].weights.len() {
+                let rng_num = rng.gen_range(-0.1..0.1);
+                mxo.hidden_nodes_2[i].weights[j] += rng_num;
+            }
+        }
+
+        for i in 0..mxo.output_nodes.len() {
+            let rng_num = rng.gen_range(-1.0..1.0);
+            mxo.output_nodes[i].bias += rng_num;
+            for j in 0..mxo.output_nodes[i].weights.len() {
+                let rng_num = rng.gen_range(-0.1..0.1);
+                mxo.output_nodes[i].weights[j] += rng_num;
+            }
+        }
+
+        serde_json::to_string_pretty(&mxo).unwrap()
+
+
     }
 
+    pub fn run(mut mx: Matrix2442, inputs: Vec<f64>) -> Vec<bool> {
+
+
+        for i in 0..mx.input_nodes.len() {
+            if mx.input_nodes.len() != inputs.len() {
+                panic!("inputs and matrix input nodes are not the same length");
+            }
+            mx.input_nodes[i].value = inputs[i] as f32;
+        }
+
+        for i in 0..mx.hidden_nodes_1.len() {
+            mx.hidden_nodes_1[i].value += mx.hidden_nodes_1[i].bias;
+        for j in 0..mx.hidden_nodes_1[i].weights.len() {
+                mx.hidden_nodes_1[i].value += mx.input_nodes[j].value * mx.hidden_nodes_1[i].weights[j];
+        }
+        }
+
+        for i in 0..mx.hidden_nodes_2.len() {
+            mx.hidden_nodes_2[i].value += mx.hidden_nodes_2[i].bias;
+        for j in 0..mx.hidden_nodes_2[i].weights.len() {
+                mx.hidden_nodes_2[i].value += mx.hidden_nodes_1[j].value * mx.hidden_nodes_2[i].weights[j];
+        }
+        }
+
+        for i in 0..mx.output_nodes.len() {
+            mx.output_nodes[i].value += mx.output_nodes[i].bias;
+        for j in 0..mx.output_nodes[i].weights.len() {
+                mx.output_nodes[i].value += mx.hidden_nodes_2[j].value * mx.output_nodes[i].weights[j];
+        }
+        }
+
+    // outputs include ["move_left", "move_right", "jump", "attack_up", "attack_down", "attack forwards"]
+    let mut action: Vec<bool> = vec![false, false, false, false, false, false];
+
+    if mx.output_nodes.len() == 2 {
+        action[0] = mx.output_nodes[0].value >= 0.0;
+        action[1] = mx.output_nodes[1].value >= 0.0;
+    }
+
+
+    action
+    
+
 }
 
+}
